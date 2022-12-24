241a0a5d81982dc79af92b285b3032bf687ff794
AlexStorm <info@alexstorm.ru>
2022-12-24 11:37:34 +0530

    deep refactor

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/utils.module';
 export * from './lib/click-outside.directive';
 export * from './lib/base-dir.pipe';
 export * from './lib/model';
+export * from './lib/declared.service';
 
 
---
+++ src/lib/declared.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {filter, firstValueFrom, map, Observable, Subject, tap} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DeclaredService {
+    private components: { [module: string]: { [componentName: string]: any } };
+    addEvents: Subject<string> = new Subject<string>();
+
+    constructor() {
+        this.components = {};
+    }
+
+
+    getComponent(module: string, componentName: string): Promise<any> {
+        if (module in this.components) {
+
+            const comp= this.components[module][componentName]
+            console.log("GET COMP11",comp)
+            return Promise.resolve(comp)
+        } else {
+            console.log("GET COMP1")
+           return  firstValueFrom(this.addEvents.asObservable().pipe(tap(item=>console.log("ITEMS LAD",item))).pipe(filter(name => name == module))
+                .pipe(map(n=>this.components[module][componentName]))
+               .pipe(tap(item=>console.log("COMP LAD",item))))
+        }
+    }
+
+    addComps(module: string, list: any[]) {
+        console.log("ADD COMP")
+        this.components[module] = {}
+        for (const comp of list) {
+            this.components[module][comp.name] = comp;
+        }
+        this.addEvents.next(module);
+    }
+}
