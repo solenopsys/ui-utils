fcc8ddf822af7f44995a12d668e5d2d9094bc8fa
AlexStorm <info@alexstorm.ru>
2023-02-26 07:51:11 +0530

    name refactoring

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/packages/uimatrix/utils',
+  coverageDirectory: '../../../coverage/packages/uimatrix/ui-utils',
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
